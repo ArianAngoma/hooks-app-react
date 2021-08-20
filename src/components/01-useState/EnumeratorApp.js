5d85015a0597ce761b7366e144bde87a55c76146
unknown <arian.angoma@tecsup.edu.pe>
2021-08-20 00:48:42 -0500

    Start project Hooks

---
+++ src/components/01-useState/EnumeratorApp.js
@@ -0,0 +1,14 @@
+const EnumeratorApp = () => {
+    return (
+        <>
+            <h1>Counter {0}</h1>
+            <hr/>
+
+            <button className="btn btn-primary">
+                +1
+            </button>
+        </>
+    )
+}
+
+export default EnumeratorApp;
