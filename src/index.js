f89bc02a73e942a4f340db7516f1dad8d917e9fe
unknown <arian.angoma@tecsup.edu.pe>
2021-08-22 16:59:16 -0500

    Custom hook useFetch

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MultipleCustomHook from "./components/03-examples/MultipleCustomHook";
 // import HooksApp from "./HooksApp";
 // import EnumeratorApp from "./components/01-useState/EnumeratorApp";
 // import EnumeratorWithCustomHook from "./components/01-useState/EnumeratorWithCustomHook";
 // import SimpleForm from "./components/02-useEffect/SimpleForm";
-import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
+// import FormWithCustomHook from "./components/02-useEffect/FormWithCustomHook";
 
 ReactDOM.render(
     // Hook useState
@@ -17,7 +18,8 @@ ReactDOM.render(
     // <SimpleForm/>,
 
     // Form with custom hook
-    <FormWithCustomHook/>,
-document.getElementById('root')
-)
-;
+    // <FormWithCustomHook/>,
+
+    <MultipleCustomHook/>,
+    document.getElementById('root')
+);
