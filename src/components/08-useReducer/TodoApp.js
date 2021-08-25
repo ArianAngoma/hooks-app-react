864e6f3016f75bc1275ef97160ee0b760a31e36b
unknown <arian.angoma@tecsup.edu.pe>
2021-08-25 00:20:56 -0500

    View Todo List

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -2,11 +2,11 @@ import './styles.css';
 import {useReducer} from "react";
 import todoReducer from "./todoReducer";
 
-const initialState = {
+const initialState = [{
     id: new Date().getTime(),
     description: 'Aprender React',
     done: false
-}
+}]
 
 const TodoApp = () => {
     const [todos] = useReducer(todoReducer, initialState);
@@ -14,14 +14,41 @@ const TodoApp = () => {
 
     return (
         <div>
-            <h1>TodoApp</h1>
+            <h1>TodoApp ({todos.length})</h1>
             <hr/>
 
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Arian</li>
-            </ul>
+            <div className='row'>
+                <div className='col-7'>
+                    <ul className='list-group list-group-flush'>
+                        {
+                            todos.map((todo, i) => (
+                                <li
+                                    key={todo.id}
+                                    className='list-group-item'>
+                                    <p className='text-center'>{i + 1}. {todo.description}</p>
+                                    <button className='btn btn-danger'>Borrar</button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className='col-5'>
+                    <h4>Agregar Todo</h4>
+                    <hr/>
+
+                    <form>
+                        <input type='text'
+                               name='description'
+                               className='form-control'
+                               placeholder='Tarea'
+                               autoComplete='off'/>
+
+                        <button className='btn btn-outline-primary mt-1 w-100'>
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
