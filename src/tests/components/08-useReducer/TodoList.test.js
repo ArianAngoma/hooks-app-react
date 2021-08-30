d9278abbb6ee34d54ba3705cfc756df9c6e4ca34
unknown <arian.angoma@tecsup.edu.pe>
2021-08-30 03:17:38 -0500

    Test TodoList

--- src/tests/components/08-useReducer/TodoList.test.js
+++ src/tests/components/08-useReducer/TodoList.test.js
@@ -17,6 +17,8 @@ describe('Test on component <TodoList/>', () => {
     });
 
     test('Should have two <TodoListItem/>', () => {
-        console.log(wrapper.find('TodoListItem').length);
+        expect(wrapper.find('TodoListItem').length).toBe(demoTodos.length);
+        // console.log(wrapper.find('TodoListItem').at(0).props());
+        expect(wrapper.find('TodoListItem').at(0).prop('handleDelete')).toEqual(expect.any(Function));
     });
 })
