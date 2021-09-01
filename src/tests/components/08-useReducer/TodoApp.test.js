d204f12da97c76f68b4bf66c4d9ef86e0587e4f3
unknown <arian.angoma@tecsup.edu.pe>
2021-09-01 18:36:23 -0500

    Test localStore

--- src/tests/components/08-useReducer/TodoApp.test.js
+++ src/tests/components/08-useReducer/TodoApp.test.js
@@ -6,6 +6,8 @@ import {act} from "@testing-library/react";
 describe('Test on <TodoApp/>', () => {
     const wrapper = shallow(<TodoApp/>);
 
+    Storage.prototype.setItem = jest.fn();
+
     test('Should display correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
@@ -19,5 +21,12 @@ describe('Test on <TodoApp/>', () => {
         });
 
         expect(wrapper.find('h1').text().trim()).toBe('TodoApp (2)');
+        expect(localStorage.setItem).toHaveBeenCalledTimes(2);
+    });
+
+    test('Should delete a ToDo', () => {
+        wrapper.find('TodoAdd').prop('handleAddTodo')(demoTodos[0]);
+        wrapper.find('TodoList').prop('handleDelete')(demoTodos[0].id);
+        expect(wrapper.find('h1').text().trim()).toBe('TodoApp (0)');
     });
 })
